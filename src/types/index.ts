ef096d2436563808f13619963bfe80ba0864a7b7
Nicholas Shellabarger <temptemp3@users.noreply.github.com>
2024-11-08 10:23:19 -0700

    add rankings type

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface RankingI {
+  id: string;
+  name: string;
+  image: string;
+  contractId: string;
+  volume: number;
+  floorPrice: number;
+  items: number;
+  owners: number;
+  sales: number;
+} 
